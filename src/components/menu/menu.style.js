a87d1b2d081e1a762f86f354e206b12c298750dc
Andrey Perov <jsperov@gmail.com>
2018-12-14 00:09:10 +0300

    fix

--- src/components/menu/menu.style.js
+++ src/components/menu/menu.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MenuBox = styled.ul`
   margin: 0;
-  background: ${props => props.theme.colors.darkBlue};
+  background: ${({ theme }) => theme.colors.darkBlue};
 
   .main-menu__link {
     padding: 3px 10px;
